51e6eb4fb660da94122c93f0525ec791c929f937
VinitashokPatil <patilavinit@gmail.com>
2019-08-03 21:40:43 -0500

    first checkin

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SecondAttemptFirstSelenium
+{
+    class Program
+    {
+        private IWebDriver driver = new ChromeDriver();
+
+        //static void Main(string[] args)
+        //{
+        // IWebDriver driver = new ChromeDriver();
+        //driver.Navigate().GoToUrl("http://www.google.com");
+        // }
+
+        [SetUp]
+        public void Initialize()
+        {
+            if (driver==null)
+                driver = new ChromeDriver();
+
+            Console.WriteLine("Navigate to site");
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            Console.WriteLine("Execute Test");
+            SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+            SeleniumSetMethods.EnterText(driver, "Initial", "Selenium", "Name");
+            SeleniumSetMethods.Click(driver, "Save", "Name");
+
+
+            //IWebElement element = driver.FindElement(By.Name("q"));
+            //element.SendKeys("selenium");
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next Test");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Console.WriteLine("driver close");
+            driver.Close();
+            driver = null;
+        }
+
+
+    }
+}
---
+++ SeleniumSetMethods.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondAttemptFirstSelenium
+{
+    class SeleniumSetMethods
+    {
+        //Enter Text
+        public static void EnterText(IWebDriver driver, string element, string value, string locatorType)
+        {
+            if (locatorType == "id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            }
+            if (locatorType == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            }
+        }
+
+        //Click Button CheckBox Options
+        public static void Click(IWebDriver driver, string element, string locatorType)
+        {
+            if (locatorType == "id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+            }
+            if (locatorType == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click();
+            }
+        }
+        //Select
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string locatorType)
+        {
+            if (locatorType == "id")
+            {
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            }
+            if (locatorType == "Name")
+            {
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            }
+        }
+    }
+}
